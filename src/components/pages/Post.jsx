4e87067072ef81739d44d3cc3496c6c508ed2769
AvijitSam <134062474+AvijitSam@users.noreply.github.com>
2023-08-22 17:18:26 +0530

    Update Post.jsx

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -18,7 +18,28 @@ export default function Post() {
     .then(data=>setPosts(data.data))
     .catch(err =>console.log(err));
   },[])
- 
+  const onDelete = (id) => {
+    fetch(`http://demoyourprojects.com:5085/post/${id}`, {
+      method: 'DELETE',
+      headers: myHeaders,
+    })
+    .then((res)=>res.json())
+    .then(data=>{
+      return data;
+     
+      //const index = posts.findIndex((post)=>post._id == id);
+      //posts.splice(index,1);
+    }).then(results=>{
+      fetch(`http://demoyourprojects.com:5085/post`, {
+        method: 'GET',
+        headers: myHeaders,
+      }).then((res)=>res.json())
+      .then(data2=>setPosts(data2.data))
+      .catch(err =>console.log(err));
+      alert('Post deleted successfully!');
+    })
+    .catch(err =>console.log(err));
+  }
     return (
       <div className="login-container">
         <h1>Posts List</h1>
@@ -43,7 +64,7 @@ export default function Post() {
                       <td> 
                         <Link to={`/post/view/${post._id}`} className="btn btn-primary" style={{marginLeft:"5px"}}>View</Link>
                         <Link to={`/post/update/${post._id}`} className="btn btn-warning" style={{marginLeft:"5px"}}>Edit</Link>
-                        <Button className='btn btn-danger' style={{marginLeft:"5px"}}>Delete</Button>
+                        <Button className='btn btn-danger' style={{marginLeft:"5px"}} onClick={() => onDelete(post._id)}>Delete</Button>
                       </td>
                     </tr>
                   )):(<tr>
